b1b814a0c099155a9ced5580fab4aa65b7afe542
CRThu <yummycarrot@qq.com>
2020-09-14 11:14:56 +0800

    update testbench

--- vivado/XC7Z010_QDUC/XC7Z010_QDUC.srcs/sim_1/new/scrambler_tb.v
+++ vivado/XC7Z010_QDUC/XC7Z010_QDUC.srcs/sim_1/new/scrambler_tb.v
@@ -48,20 +48,28 @@ module scrambler_tb;
         #5 clk_in = ~clk_in;
         
     integer i;
+    reg [256-1:0] data_in_vec = 256'h416C7068610D0A427261766F0D0A436861726C69650D0A44656C74610D0A4563;
+    reg [256-1:0] data_out_vec = 256'h0;
+    
     initial
     begin
         #30 reset_n = 1'b0;
         #30 reset_n = 1'b1;
-       
-        #20 data_vaild = 1'b1;
         
-        for(i=0;i<127;i=i+1)
+        #6 data_vaild = 1'b0;
+        
+        for(i=0;i<256;i=i+1)
         begin
-            #10 $write("%b ",data_out);
-            if(i%16 == 15)
-                $write("\n");
+            #7 data_vaild = 1'b1;
+            data_in = data_in_vec[256-1-i];
+            
+            #3 data_vaild = 1'b0;
+            data_out_vec[256-1-i] = data_out;
         end
+        
+        $display("%h",data_out_vec);
         $write("\n");
+        
         $stop;
     end
     
