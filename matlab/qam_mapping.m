63fc5b378dcb09a3878994091ac5bb7bf7d75641
CRThu <yummycarrot@qq.com>
2020-09-27 00:04:52 +0800

    fix bugs in qam_mapping.m

--- matlab/qam_mapping.m
+++ matlab/qam_mapping.m
@@ -11,6 +11,9 @@ ANALOG_WIDTH = 16;
 
 % testbench vector symbol length
 TEST_SYMBOL_VEC_LEN = 16;
+USE_RAND_VEC = 0;
+SPECIFY_VEC = ['A337666075EF9992BF7DE1DD110BDC92'];
+%SPECIFY_VEC = ['776F2E1A538E3BE8D10B7ED023199554'];
 
 % display label control
 DISPLAY_LABEL = 1;
@@ -18,7 +21,7 @@ DISPLAY_LABEL = 1;
 % Configure LUT .coe Path
 DATA_PATH = './data/';
 LUT_FILE_OUT = 'quantized_level_lut.coe';
-TEST_VEC_OUT = 'qam_mapping';   % qam_mapping_A337.txt
+TEST_VEC_OUT = 'qam_mapping.txt';   % qam_mapping.txt
 
 % calculate parameters
 DATA_IQ_WIDTH = DATA_WIDTH/2;
@@ -27,17 +30,21 @@ DAC_MIN = -power(2,ANALOG_WIDTH-1);
 DAC_MAX = power(2,ANALOG_WIDTH)-1;
 POS_MAX = power(2,DATA_IQ_WIDTH)-1;
 
-% generate rand data
-rand_data_bin = round(randi([0 1],1,TEST_SYMBOL_VEC_LEN * DATA_WIDTH));
-rand_data_bits = reshape(double(rand_data_bin).',4,TEST_SYMBOL_VEC_LEN * DATA_WIDTH/4).';
-rand_data_hex = bi2de(rand_data_bits,'left-msb');
-data_in_vec = dec2hex(rand_data_hex)';
-
-data_in_vec = ['A337666075EF9992BF7DE1DD110BDC92'];
-%data_in_vec = ['776F2E1A538E3BE8D10B7ED023199554'];
+if USE_RAND_VEC == 1
+    % generate rand data
+    rand_data_bin = round(randi([0 1],1,TEST_SYMBOL_VEC_LEN * DATA_WIDTH));
+    rand_data_bits = reshape(double(rand_data_bin).',4,TEST_SYMBOL_VEC_LEN * DATA_WIDTH/4).';
+    rand_data_hex = bi2de(rand_data_bits,'left-msb');
+    data_in_vec = dec2hex(rand_data_hex)';
+else
+    % specify vector
+    data_in_vec = SPECIFY_VEC;
+    rand_bits = de2bi(hex2dec(data_in_vec.'),'left-msb');
+    rand_data_bin = reshape(double(rand_bits).',1,TEST_SYMBOL_VEC_LEN * DATA_WIDTH);
+end
 
 % generate test vec file
-test_vec_fid = fopen([DATA_PATH TEST_VEC_OUT '_' data_in_vec(1:4) '.txt'],'w');
+test_vec_fid = fopen([DATA_PATH TEST_VEC_OUT],'w');
 
 fprintf('%dQAM MAPPING\n',power(2,DATA_WIDTH));
 fprintf('data_in_vec = %s\n',data_in_vec);
@@ -49,33 +56,30 @@ Q_out_vec = zeros(1,TEST_SYMBOL_VEC_LEN);
 label_out_vec = cell([1,TEST_SYMBOL_VEC_LEN]);
 
 for i=1:TEST_SYMBOL_VEC_LEN
-    % get data_L and data_H
-    if DATA_WIDTH <= 4
-        % one byte per symbol
-        data = hex2dec(data_in_vec(i));
-    elseif DATA_WIDTH <= 8
-        % two bytes per symbol
-        data = hex2dec(data_in_vec(i*2-1:i*2));
-    else
-        fprintf('ERROR:unexpected data\n')
-    end
-
+    % get data
+    rand_data_i = rand_data_bin((i-1)*DATA_WIDTH+1:(i-1)*DATA_WIDTH+DATA_WIDTH);
+    data = bin2dec(num2str(rand_data_i')');
+    
     % calculate data_H and L bits
-    %fprintf('DATA:%s\n',dec2bin(data,DATA_WIDTH));
+    %fprintf('DATA:%s\t',dec2bin(data,DATA_WIDTH));
     data_H = bitand(bitshift(data, -DATA_IQ_WIDTH),DATA_IQ_MAX);
     data_L = bitand(data, DATA_IQ_MAX);
-    %fprintf('DATA:%s %s\t',dec2bin(data_H,DATA_IQ_WIDTH),dec2bin(data_L,DATA_IQ_WIDTH));
-    fprintf('DATA:0x%s%s\t',dec2hex(data_H,DATA_IQ_WIDTH/4),dec2hex(data_L,DATA_IQ_WIDTH/4));
-    fprintf(test_vec_fid,'DATA:0x%s%s\t',dec2hex(data_H,DATA_IQ_WIDTH/4),dec2hex(data_L,DATA_IQ_WIDTH/4));
-
+    if mod(DATA_IQ_WIDTH,4) == 0
+        fprintf('DATA:0x%s%s\t',dec2hex(data_H,DATA_IQ_WIDTH/4),dec2hex(data_L,DATA_IQ_WIDTH/4));
+        fprintf(test_vec_fid,'DATA:0x%s%s\t',dec2hex(data_H,DATA_IQ_WIDTH/4),dec2hex(data_L,DATA_IQ_WIDTH/4));
+    else
+        fprintf('DATA:%s%s\t',dec2bin(data_H,DATA_IQ_WIDTH),dec2bin(data_L,DATA_IQ_WIDTH));
+        fprintf(test_vec_fid,'DATA:%s%s\t',dec2bin(data_H,DATA_IQ_WIDTH),dec2bin(data_L,DATA_IQ_WIDTH));
+    end
+    
     % relative position
     I_pos = DATA_IQ_MAX - bin2dec(gray2bin(dec2bin(data_L)));
     Q_pos = DATA_IQ_MAX - bin2dec(gray2bin(dec2bin(data_H)));
     %fprintf('I_pos:%2d  Q_pos:%2d\t',I_pos,Q_pos);
 
     % quantized level
-    I_out = DAC_MIN + DAC_MAX / POS_MAX * I_pos;
-    Q_out = DAC_MIN + DAC_MAX / POS_MAX * Q_pos;
+    I_out = fix(DAC_MIN + DAC_MAX / POS_MAX * I_pos);
+    Q_out = fix(DAC_MIN + DAC_MAX / POS_MAX * Q_pos);
     fprintf('I:%6d  Q:%6d\n',I_out,Q_out);
     fprintf(test_vec_fid,'I:%6d  Q:%6d\n',I_out,Q_out);
 
@@ -101,7 +105,7 @@ fprintf(coe_fid,'memory_initialization_radix=10;\n');
 fprintf(coe_fid,'memory_initialization_vector=\n');
 ch = [',' char(10)];
 for i=0:DATA_IQ_MAX
-    quantized_level_lut(i+1) = DAC_MIN + DAC_MAX / POS_MAX * i;
+    quantized_level_lut(i+1) = fix(DAC_MIN + DAC_MAX / POS_MAX * i);
     if i == DATA_IQ_MAX
         ch = ';';
     end
@@ -131,3 +135,5 @@ function Bin=gray2bin(Gray)
         Bin(i+1) = dec2bin(xor(tmp1, tmp2));
     end
 end
+
+% EOF
--- vivado/XC7Z010_QDUC/XC7Z010_QDUC.sim/sim_1/behav/xsim/xsim.dir/qam_mapping_tb_behav/webtalk/xsim_webtalk.tcl
+++ vivado/XC7Z010_QDUC/XC7Z010_QDUC.sim/sim_1/behav/xsim/xsim.dir/qam_mapping_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/Projects/Carrot-Inside-RF-Transmitter/vivado/XC7Z010_QDUC/XC7Z010_QDUC.sim/sim_1/behav/xsim/xsim.dir/qam_mapping_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sat Sep 26 22:53:07 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sun Sep 27 00:02:27 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "4bad9526-75ac-46de-a82b-c9767d2800b1" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "474f1a9697b74573a054b532fb577ad6" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "58" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "61" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2012 R2 or Windows 8.1" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-4770HQ CPU @ 2.20GHz" -context "user_environment"
@@ -28,5 +28,5 @@ webtalk_add_data -client xsim -key runtime -value "287 ns" -context "xsim\\usage
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.09_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "6676_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2298050998 -regid "" -xml D:/Projects/Carrot-Inside-RF-Transmitter/vivado/XC7Z010_QDUC/XC7Z010_QDUC.sim/sim_1/behav/xsim/xsim.dir/qam_mapping_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Projects/Carrot-Inside-RF-Transmitter/vivado/XC7Z010_QDUC/XC7Z010_QDUC.sim/sim_1/behav/xsim/xsim.dir/qam_mapping_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Projects/Carrot-Inside-RF-Transmitter/vivado/XC7Z010_QDUC/XC7Z010_QDUC.sim/sim_1/behav/xsim/xsim.dir/qam_mapping_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 1817681157 -regid "" -xml D:/Projects/Carrot-Inside-RF-Transmitter/vivado/XC7Z010_QDUC/XC7Z010_QDUC.sim/sim_1/behav/xsim/xsim.dir/qam_mapping_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Projects/Carrot-Inside-RF-Transmitter/vivado/XC7Z010_QDUC/XC7Z010_QDUC.sim/sim_1/behav/xsim/xsim.dir/qam_mapping_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Projects/Carrot-Inside-RF-Transmitter/vivado/XC7Z010_QDUC/XC7Z010_QDUC.sim/sim_1/behav/xsim/xsim.dir/qam_mapping_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
