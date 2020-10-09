88a15f8fe66c365b0b1ebf443d7463181b8a4692
CRThu <yummycarrot@qq.com>
2020-10-09 23:32:58 +0800

    update halfband filter

--- vivado/XC7Z010_QDUC/XC7Z010_QDUC.srcs/sources_1/new/halfband_filter.v
+++ vivado/XC7Z010_QDUC/XC7Z010_QDUC.srcs/sources_1/new/halfband_filter.v
@@ -20,7 +20,44 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module halfband_filter(
-
+module halfband_filter
+    #(parameter DATA_IN_WIDTH = 16,
+    parameter DATA_OUT_WIDTH = 16)(
+    input clk_in,
+    input [DATA_IN_WIDTH-1:0] data_in_I,
+    input [DATA_IN_WIDTH-1:0] data_in_Q,
+    input data_in_valid,
+    output data_in_ready,
+    output [DATA_OUT_WIDTH-1:0] data_out_I,
+    output [DATA_OUT_WIDTH-1:0] data_out_Q,
+    output data_out_valid
     );
+    
+    `ifdef USE_XILINX_IP
+        // USE XILINX IP
+        
+        // WARNING : START UP LATENCY = 20 CLKs
+        
+        // AXI4 Stream Port Structure
+        // Data Field            Type
+        // Q    PATH_1(31:16)    fix16_0
+        // I    PATH_0(15: 0)    fix16_0
+        
+        wire [DATA_IN_WIDTH*2-1:0] data_in;
+        wire [DATA_OUT_WIDTH*2-1:0] data_out;
+        
+        assign data_in = {data_in_Q,data_in_I};
+        assign {data_out_Q,data_out_I} = data_out;
+        
+        //----------- Begin Cut here for INSTANTIATION Template ---// INST_TAG
+        fir_compiler_1 fir_compiler_1_inst (
+          .aclk                 (clk_in),           // input wire aclk
+          .s_axis_data_tvalid   (data_in_valid),    // input wire s_axis_data_tvalid
+          .s_axis_data_tready   (data_in_ready),    // output wire s_axis_data_tready
+          .s_axis_data_tdata    (data_in),          // input wire [31 : 0] s_axis_data_tdata
+          .m_axis_data_tvalid   (data_out_valid),   // output wire m_axis_data_tvalid
+          .m_axis_data_tdata    (data_out)          // output wire [31 : 0] m_axis_data_tdata
+        );
+        // INST_TAG_END ------ End INSTANTIATION Template ---------
+    `endif
 endmodule
