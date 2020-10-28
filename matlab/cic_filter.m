37abbe5ba84222bf839cc01364147fb8f8d08163
CRThu <yummycarrot@qq.com>
2020-10-28 17:04:03 +0800

    update cic compensator filter

--- matlab/cic_filter.m
+++ matlab/cic_filter.m
@@ -8,15 +8,25 @@ close all
 % use functions in utility folder
 addpath(genpath('./utility'));
 
+% Configure Filter .coe Path
+COE_PATH = './coe/';
+CICCOMP0_COE_OUT = 'cic_filter_comp_0.coe';
+
+% COE width
+COE_WIDTH = 16;
+
 % CIC Filter Configure
 CIC_R = 8;      % Rate Change
 CIC_N = 5;      % Number of Stages
 CIC_M = 1;      % Differential Delay
 
-DISPLAY_LEN = 128;
+% h(t)
+ht_GEN = 0;     % 0:Signal; 1:h(t);
+ht_ZEROS = 10;
+ht_CIC = [1,5,15,35,70,126,210,330,490,690,926,1190,1470,1750,2010,2226,2380,2460,2460,2380,2226,2010,1750,1470,1190,926,690,490,330,210,126,70,35,15,5,1];
 
 % test signals
-fs = 1024;
+fs = 64;
 Nsample = fs;
 t = 0:1/Nsample:1/fs*(Nsample-1);
 
@@ -30,8 +40,16 @@ sin_sum = sin2;
 % --> Comb -> ... -> Comb --> ¡ÁR --> Int -> ... -> Int -->
 
 % DATA
-DATA_IN = floor(sin_sum);
+if ht_GEN == 0
+    DATA_IN = floor(sin_sum);
+else
+    DATA_IN = [1,zeros(1,ht_ZEROS)];
+end
+
 DATA_LEN = length(DATA_IN);
+
+DISPLAY_LEN = DATA_LEN;
+%DISPLAY_LEN = 128;
 
 % N Stages Comb
 comb_net = zeros(1,CIC_N+1);
@@ -79,8 +97,51 @@ for i=1:DATA_LEN*CIC_R
     %display(int_reg);
 end
 
-% Display
-f=figure(1);
+% CIC Compensator Filter
+% Reference: https://ww2.mathworks.cn/help/dsp/ref/fdesign.ciccomp.html
+h_ciccomp = fdesign.ciccomp;
+set(h_ciccomp, 'NumberOfSections', CIC_N, 'DifferentialDelay', CIC_M);
+set(h_ciccomp, 'ap', 0.1, 'ast' , 60);
+set(h_ciccomp, 'fp', 0.48, 'fst' , 0.7);
+cicComp = design(h_ciccomp,'equiripple','SystemObject',true);
+ht_CIC_Comp = cicComp.Numerator;
+%fvtool(ht_CIC_Comp)
+
+% Write COE Fiile
+filter_coe_writer([COE_PATH CICCOMP0_COE_OUT],COE_WIDTH,ht_CIC_Comp);
+
+% Freq Response
+[H1,w1]=freqz(ht_CIC);
+[H2,w2]=freqz(ht_CIC_Comp);
+w2_len = length(w2);
+w2_ext = [w2;zeros(w2_len*(CIC_R-1),1)];
+H2_ext = [H2;zeros(w2_len*(CIC_R-1),1)];
+for i=1:CIC_R-1
+    w2_ext(w2_len*i+1:w2_len*(i+1)) = w2+pi*i;
+    if mod(i,2)==0
+        H2_ext(w2_len*i+1:w2_len*(i+1)) = H2;
+    else
+        H2_ext(w2_len*i+1:w2_len*(i+1)) = flipud(H2);
+    end
+end
+max_H1_dB = db(max(H1));
+figure(1);
+subplot(2,1,1);
+plot(w1.*CIC_R/pi,db(H1)-max_H1_dB,w2_ext/pi,db(H2_ext));
+axis([0 0.65 -10 7.5]);
+grid on;
+ylabel('Normalized Magnitude (dB)');
+legend('CIC Interpolator','CIC Compensator');
+subplot(2,1,2);
+plot(w1.*CIC_R/pi,db(H1)-max_H1_dB,w2_ext/pi,db(H2_ext));
+axis([0 CIC_R -150 7.5]);
+grid on;
+xlabel('Normalized Frequency (\times\pi rad/sample)');
+ylabel('Normalized Magnitude (dB)');
+legend('CIC Interpolator','CIC Compensator');
+
+% Display Signal
+f=figure(2);
 subplot(3,1,1);
 stairs(DATA_IN(1:DISPLAY_LEN));
 subplot(3,1,2);
@@ -88,6 +149,14 @@ stairs(comb_data_out(1:DISPLAY_LEN));
 subplot(3,1,3);
 stairs(int_data_out(1:DISPLAY_LEN*4));
 xlabel('Samples');
+
+% Display h(t)
+if ht_GEN == 1
+    ht_CIC = int_data_out(int_data_out~=0);
+    fprintf('h(t) = ');
+    fprintf('%d;',ht_CIC);
+    fprintf('\nPlease Update ht_CIC in .m file.\n');
+end
 
 % Override Y Label
 OverrideYLabel(f,0);
